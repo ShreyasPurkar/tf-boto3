f1670fc075f17acab22f57e97d48f2b852590de6
Shreyas Vijay Purkar <purkar.s@northeastern.edu>
2025-04-16 22:49:31 -0400

    Update terraform file structure

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,7 @@
-# Generate UUID for bucket name
+# Generate UUID for bucket name to have unique bucket names globally unique across all AWS accounts.
 resource "random_uuid" "bucket_name_uuid" {}
 
-# Create S3 bucket
+# Create S3 bucket with the generated UUID as the name
 resource "aws_s3_bucket" "bucket" {
   bucket = random_uuid.bucket_name_uuid.result
 
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-# The bucket is set to private.
+# Make the bucket ownership enforced
 resource "aws_s3_bucket_ownership_controls" "bucket_owner_control" {
   bucket = aws_s3_bucket.bucket.id
 
@@ -21,7 +21,7 @@ resource "aws_s3_bucket_ownership_controls" "bucket_owner_control" {
   }
 }
 
-# The bucket's public access is blocked
+# Block the bucket from being publicly accessible
 resource "aws_s3_bucket_public_access_block" "bucket_block_public_access" {
   bucket                  = aws_s3_bucket.bucket.id
   block_public_acls       = true
@@ -30,13 +30,28 @@ resource "aws_s3_bucket_public_access_block" "bucket_block_public_access" {
   restrict_public_buckets = true
 }
 
-# The bucket is encrypted with AES256.
+# Encrypt the bucket using server-side encryption with AES-256 encryption
 resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
   bucket = aws_s3_bucket.bucket.id
 
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
+    }
+  }
+}
+
+# The bucket has a lifecycle rule to transition objects to the STANDARD_IA storage class after 30 days.
+resource "aws_s3_bucket_lifecycle_configuration" "bucket_lifecycle_rule" {
+  bucket = aws_s3_bucket.bucket.id
+
+  rule {
+    id     = "transition-to-ia"
+    status = "Enabled"
+
+    transition {
+      days          = var.s3_transition_days
+      storage_class = var.s3_transition_storage_class
     }
   }
 }
